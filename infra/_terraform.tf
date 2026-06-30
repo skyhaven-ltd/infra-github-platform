bdfb7a6f18f7e449af7817f546fb595af47bb101
Liam Goodchild <liamgoodchild12@hotmail.co.uk>
2026-06-30 10:31:00 +0100

    Trim stale provider, backend and tfvars comments

--- infra/_terraform.tf
+++ infra/_terraform.tf
@@ -8,12 +8,5 @@ terraform {
     }
   }
 
-  # Remote state in Azure Storage. Settings are supplied at init time via
-  # -backend-config (locally for the one-off migration, by the workflow in CI):
-  #   resource_group_name  = rg-tfs-platform-prd-uks-01
-  #   storage_account_name = sttfsplatformprduks01
-  #   container_name       = infra-github-platform
-  #   key                  = terraform.tfstate
-  #   subscription_id      = <platform subscription>
   backend "azurerm" {}
 }
